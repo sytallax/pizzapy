5e39a785cf3f2068779e240a522675057ae81c8b
sytallax <sytallax@gmail.com>
2025-04-20 16:16:49 -0500

    feat: finish menu model and query

--- pizzapy/dominos.py
+++ pizzapy/dominos.py
@@ -1,11 +1,13 @@
 from collections.abc import Generator
 from enum import Enum
 from json import JSONDecodeError
+from typing import Any
 
 import httpx
 import structlog
 
 from pizzapy.address import Address
+from pizzapy.menu import Menu, MenuCategory, MenuLineItem, MenuProduct
 from pizzapy.store import PickupType, Store
 
 logger = structlog.stdlib.get_logger(__name__)
@@ -14,9 +16,9 @@ logger = structlog.stdlib.get_logger(__name__)
 class DominosApiEndpoints(Enum):
     """Collection of valid Dominos API endpoint URLs"""
 
-    FIND_STORE = "https://order.dominos.com/power/store-locator?s=${address_line_one}&c=${address_line_two}&type=${pickup_type}"
+    FIND_STORE = "https://order.dominos.com/power/store-locator?s={address_line_one}&c={address_line_two}&type={pickup_type}"
     GET_MENU = (
-        "https://order.dominos.com/power/store/${store_id}/menu?lang=en&structured=true"
+        "https://order.dominos.com/power/store/{store_id}/menu?lang=en&structured=true"
     )
 
 
@@ -101,7 +103,7 @@ class DominosApiConnector:
                 return store
         return None
 
-    def get_menu_for_store(self, store: Store) -> None:
+    def get_menu_for_store(self, store: Store) -> Menu | None:
         endpoint = DominosApiEndpoints.GET_MENU
         url = endpoint.value.format(store_id=store.id_)
         with httpx.Client() as client:
@@ -110,5 +112,101 @@ class DominosApiConnector:
         try:
             data = response.json()
         except JSONDecodeError:
-            logger.error("Failed to parse JSON from Dominos", response=response)
-            return
+            logger.error(
+                "Failed to parse JSON from Dominos", response=response, url=response.url
+            )
+            return None
+
+        categories: list[dict[str, Any]] = (
+            data.get("Categorization", {}).get("Food", {}).get("Categories", [])
+        )
+        products: dict[str, dict[str, Any]] = data.get("Products", {})
+        variants = data.get("Variants", {})
+        cupons = data.get("Cupons", {})
+
+        if not any((categories, products, variants, cupons)):
+            logger.error("Could not parse menu", raw_menu=data)
+            return None
+
+        parsed_categories = self._parse_categories(categories)
+        parsed_products = self._parse_products(products)
+        parsed_line_items = self._parse_line_items(variants)
+
+        return Menu(
+            categories=list(parsed_categories),
+            products=list(parsed_products),
+            line_items=list(parsed_line_items),
+        )
+
+    def _parse_categories(self, data: list[dict[str, Any]]) -> Generator[MenuCategory]:
+        def get_all_product_codes_from_category(data: dict[str, Any]) -> Generator[str]:
+            if not {"Categories", "Code", "Name", "Description", "Products"}.issubset(
+                data.keys()
+            ):
+                logger.error("Incorrect menu category format", data=data)
+                return
+
+            categories = data.get("Categories", [])
+            products = data.get("Products", [])
+
+            if not products:
+                for subcategory in categories:
+                    yield from get_all_product_codes_from_category(subcategory)
+
+            yield from products
+
+        for category in data:
+            if not {"Categories", "Code", "Name", "Description", "Products"}.issubset(
+                category.keys()
+            ):
+                logger.error("Incorrect menu category format", data=category)
+                return
+
+            product_codes = set(get_all_product_codes_from_category(category))
+            code = category.get("Code", "")
+            name = category.get("Name", "")
+            description = category.get("Description", "")
+
+            if not any((product_codes, code, name, description)):
+                logger.error("Could not parse menu category data", raw_data=data)
+
+            yield MenuCategory(
+                code=code, name=name, description=description, products=product_codes
+            )
+
+    def _parse_products(
+        self, data: dict[str, dict[str, Any]]
+    ) -> Generator[MenuProduct]:
+        for code, product in data.items():
+            if not {"Code", "Name", "ProductType", "Description", "Variants"}.issubset(
+                product.keys()
+            ):
+                logger.error("Incorrect product format", raw_data=product)
+                return
+
+            name = product.get("Name", "")
+            description = product.get("Description", "")
+            variants = product.get("Variants", [])
+            yield MenuProduct(
+                code=code, name=name, description=description, variants=set(variants)
+            )
+
+    def _parse_line_items(self, data: dict[str, Any]) -> Generator[MenuLineItem]:
+        for code, variant in data.items():
+            if not {"Code", "Name", "Price", "SizeCode", "ProductCode"}.issubset(
+                variant.keys()
+            ):
+                logger.error("Incorrect variant format", data=(code, variant))
+                return
+
+            name = variant.get("Name", "")
+            product_code = variant.get("ProductCode", "")
+
+            try:
+                price = float(variant.get("Price", 0.0))
+            except ValueError:
+                logger.error(
+                    "Failed to parse price for line item", raw_line_item=variant
+                )
+                return
+            yield MenuLineItem(code, name, product_code, price)
